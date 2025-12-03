5cd90f7ac337faab00e7aadff5066da0f9cc3c81
Rodrigo <castrorodrigodev@gmail.com>
2025-12-03 01:10:44 -0300

    Environment variables

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
 from utils import clear_console_3
 
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
 
 clear_console_3()
-print("Welcome to Python !!!")
+print(f"Hi {db_user}. Welcome to Python !!!")
+
+
+print({
+    "USER": db_user, 
+    "PASSWORD": db_pass,
+})
