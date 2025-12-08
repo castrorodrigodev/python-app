8da99b7f42c8a59eb58cb5fccc3044f7422a31f5
Rodrigo <castrorodrigodev@gmail.com>
2025-12-07 22:46:02 -0300

    Generators initial

--- index.py
+++ index.py
@@ -17,13 +17,14 @@ clear_console()
 # }
 # print(result)
 
+# ************************ IF ************************
 # age = int(input("Input  your age: "))
-
 # if age < 18:
 #     print("Not Adult person")
 # else:
 #     print("Adult person")
 
+# ************************ IN - NOT IN ************************
 # users = ["Rodrigo", "Fernando", "Giovani", "Damaris", "Emanuel"]
 # print("Rodrigo" in users) # 1
 # print("Rodrigo2" in users) # 2
@@ -33,6 +34,7 @@ clear_console()
 # print("sql" not in "SQL, Python, Java, TypeScript") # 6
 # print("Sql" not in "Sql, Python, Java, TypeScript") # 7
 
+# ************************ CONTINUE ************************
 # my_name = "Rodrigo Castro"
 # counter = 0
 # for character in my_name:
@@ -42,6 +44,7 @@ clear_console()
 # print(f"My fullname has {counter} characters")
 
 
+# ************************ BREAK ************************
 # email = input("Input your email please: ")
 # for character in email:
 #     if character == "@":
@@ -51,3 +54,55 @@ clear_console()
 #     arroba = False
 
 # print(arroba)
+
+
+# ************************ GENERATORS ************************
+def generateNumberGn(limit):
+    num = 1
+    while num < limit:
+        yield num * 2
+        num = num + 1
+
+
+evenSeries = generateNumberGn(6)
+
+# for i in evenSeries:
+#     print(f"Element: {i}")
+
+print(next(evenSeries))
+# print(next(evenSeries))
+# print(next(evenSeries))
+# print(next(evenSeries))
+# print(next(evenSeries))
+
+
+def cities_gn_1(*cities):
+    for capital in cities:
+        yield capital
+
+
+def cities_gn_2(*cities):
+    for capital in cities:
+        for character in capital:
+            yield character
+
+
+def cities_gn_3(*cities):
+    for capital in cities:
+        yield from capital
+
+
+# returned_cities = cities_gn_1("Berlin", "Roma", "Bogota", "Pekin", "Hanoi")
+
+# print(next(returned_cities))  # Berlin
+# print(next(returned_cities))  # Berlin, Roma
+# print(next(returned_cities))  # Berlin, Roma, Bogota
+# print(next(returned_cities))
+# print(next(returned_cities))
+
+returned_cities_2 = cities_gn_2("Berlin", "Roma", "Bogota", "Pekin", "Hanoi")  # Similar to cities_gn_3
+print(next(returned_cities_2))  # B
+print(next(returned_cities_2))  # e
+print(next(returned_cities_2))  # r
+# print(next(returned_cities))
+# print(next(returned_cities))
